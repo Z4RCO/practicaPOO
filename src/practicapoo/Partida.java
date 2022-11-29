cc3f4d5ddaa7c9a40c32a2e99df93e83a6be0a65
crist <cristianyanez999@gmail.com>
2022-11-29 13:24:56 +0100

    dfgh

---
+++ src/practicapoo/Partida.java
@@ -0,0 +1,26 @@
+package practicapoo;
+
+public class Partida {
+
+    private int identificador;
+    private boolean regalo_de_palabra;
+    private boolean primeraLetra;
+    private int numPalabras;
+
+    public void cambiarTurno(){};
+
+    public void actualizarMarcador(){};
+
+    public boolean usar_Pista_de_Letra(){
+        return true;
+    };
+
+    public boolean usar_Pista_de_Palabra(){
+        return true;
+    };
+
+    public void info_Partida(){};
+
+
+
+}
