012f3a50c1e8a5f2442f8f7f1825674b65d48863
Z4RCO <z3rc0mrm@gmail.com>
2022-11-29 12:58:19 +0100

    Palabra (sin terminar)

---
+++ src/practicapoo/Palabra.java
@@ -0,0 +1,41 @@
+package practicapoo;
+
+public class Palabra {
+    private enum numLetras{
+        Cinco,
+        Seis
+    }
+    private char[] palabra;
+    private boolean regaloDeLetra;
+
+
+    public void jugar(){
+
+    }
+
+    private void comprobarColocadas(){
+
+    }
+
+    private void comprobarDistintaPosicion(){
+
+    }
+
+    public void mostrarIntentoResuelto(){
+
+    }
+
+    public int puntosObtenidos(){
+
+        return 0;
+    }
+
+    public void sacarPalabraAleatoria(){
+
+    }
+
+    public void secuenciaResultados(){
+
+    }
+
+}
