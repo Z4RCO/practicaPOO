11f80062dbe680c2a163286333a32707b64b53f0
Eva <alcorcon03@gmail.com>
2022-11-29 13:29:11 +0100

    AlmacenDeJugadores Entrenamiento

---
+++ src/practicapoo/Entrenamiento.java
@@ -0,0 +1,19 @@
+package practicapoo;
+
+public class Entrenamiento {
+
+    private int puntos;
+    private boolean regaloDePalabra;
+
+
+    public int mostrarPuntos(){
+
+        return 0;
+    }
+
+    public boolean usarPistaDePalabra(){
+
+        return true;
+    }
+
+}
