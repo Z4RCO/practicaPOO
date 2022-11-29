5a4d7c342ff089b7446dcd56e5f59ea4fed13c95
Eva <alcorcon03@gmail.com>
2022-11-29 14:46:56 +0100

    AlmacenDeJugadores Entrenamiento

--- src/practicapoo/palabra/Palabra.java
+++ src/practicapoo/palabra/Palabra.java
@@ -1,5 +1,9 @@
 package practicapoo.palabra;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Palabra {
     private enum numLetras{
         Cinco,
@@ -8,8 +12,12 @@ public class Palabra {
     private char[] palabra;
     private boolean regaloDeLetra;
     private Intento[] intento;
+    private int numIntentos;
 
     //TODO Hacer constructor
+
+
+
 
     public void jugar(){
 
@@ -17,18 +25,26 @@ public class Palabra {
 
     private void comprobarColocadas(){
 
+        for(int i = 0; i< palabra.length; i++){
+            char[] comp = intento[numIntentos].recogerIntento; //Palabra a comparar
+            if(palabra[i]==comp[i]){
+                // Cuadrado verde;
+            }
+        }
+
     }
 
     private void comprobarDistintaPosicion(){
+        ArrayList <Character> l = new ArrayList<Character>(palabra);
 
     }
 
     public void mostrarIntentoResuelto(){
 
+
     }
 
     public int puntosObtenidos(){
-
         return 0;
     }
 
