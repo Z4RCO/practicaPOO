231ad2907367f5084cf2f925c8fa3fd23ded1fdf
Eva <alcorcon03@gmail.com>
2022-12-04 17:03:25 +0100

    muchas cosas

--- src/practicapoo/palabra/Intento.java
+++ src/practicapoo/palabra/Intento.java
@@ -3,11 +3,11 @@ package practicapoo.palabra;
 public class Intento {
     private char[] intento;
 
+
+    public Intento() {
+        this.intento = new char[/*La longitud que el ususario seleccione en configuración*/];
+    }
     public char[] recogerIntento(){
-        return this.intento;
-    };
 
-
-
-
+    }
 }
